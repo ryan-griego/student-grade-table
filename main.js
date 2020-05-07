4fb179b36ff6b32c4d0b415ee23bbf9b2fb29813
Ryan Griego <ryangriego@gmail.com>
2020-05-07 00:19:23 +0000

    queried for table element and modified app to pass GradeTable instance

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-var newApp = new App();
+table = document.querySelector('table');
+console.log(table);
+
+var newGradeTable = new GradeTable(table);
+console.log("log newGradeTable", newGradeTable);
+
+
+var newApp = new App(this.GradeTable);
+console.log("log newApp", newApp);
 
 newApp.start();
