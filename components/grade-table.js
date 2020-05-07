e4410a1cb19a987f670873799ec1b651b98c352c
Ryan Griego <ryangriego@gmail.com>
2020-05-07 02:22:15 +0000

    Completed feature 1 of 5 - student grade table

--- components/grade-table.js
+++ components/grade-table.js
@@ -2,11 +2,27 @@
 class GradeTable  {
   constructor(tableElement) {
    this.tableElement = tableElement;
-
   }
 
   updateGrades(grades) {
+    console.log(this.tableElement);
+    var tbody = this.tableElement.querySelector('tbody');
+    $(tbody).empty();
+
+    var $header = $('<tr>' + '<td><strong>Name</strong></td>' + '<td><strong>Course</strong></td>' + '<td><strong>Grade</strong></td>' + '</tr>');
+    $('tbody').append($header);
+    $.each(grades, function (id, item) {
+      var $row = $('<tr>');
+      var $name = $('<td>' + item.name + '</td>');
+      var $course = $('<td>' + item.course + '</td>');
+      var $grade = $('<td>' + item.grade + '</td>');
+      var $rowend = $('</tr>');
+      $(tbody).append($row);
+      $($row).append($name);
+      $($row).append($course);
+      $($row).append($grade);
+      $('tbody').append($rowend);
+    });
     console.log(grades);
   }
-
 }
--- main.js
+++ main.js
@@ -4,8 +4,8 @@ console.log(table);
 var newGradeTable = new GradeTable(table);
 console.log("log newGradeTable", newGradeTable);
 
-
-var newApp = new App(this.GradeTable);
+var newApp = new App(newGradeTable);
 console.log("log newApp", newApp);
 
+newGradeTable.updateGrades();
 newApp.start();
