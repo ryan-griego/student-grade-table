385dbd4eaa8819ee1175cf478de8be301fb781f0
Ryan Griego <ryangriego@gmail.com>
2020-05-08 23:39:29 +0000

    Binded 3 methods to the App constructor

--- components/app.js
+++ components/app.js
@@ -6,6 +6,10 @@ class App {
     this.gradeTable = gradeTable;
     this.handleGetGradesError = this.handleGetGradesError.bind(this);
     this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+    this.createGrade = this.createGrade.bind(this);
+    this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
+    this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
+
   }
 
   handleGetGradesError(error) {
