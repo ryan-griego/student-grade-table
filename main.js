0da45fcc76c241b2892676e8ab321faac25190e2
Ryan Griego <ryangriego@gmail.com>
2020-05-08 22:23:35 +0000

    instantiated a new grade form while passing in the form element

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ var newGradeTable = new GradeTable(table);
 var header = document.querySelector('header');
 var newPageHeader = new PageHeader(header);
 
+var form = document.querySelector('form');
+var newGradeForm = new GradeForm(form);
+
 var newApp = new App(newGradeTable, newPageHeader);
 
 newGradeTable.updateGrades();
