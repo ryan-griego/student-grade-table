528df3e60130724054e74bc5697eeb0ba930d167
Ryan Griego <ryangriego@gmail.com>
2020-05-08 02:29:08 +0000

    added consctructor and updateAverage methods to PageHeader class

---
+++ components/page-header.js
@@ -0,0 +1,16 @@
+class PageHeader {
+  constructor(headerElement) {
+    this.headerElement = headerElement;
+  }
+
+  updateAverage(average) {
+
+
+  }
+
+  newAverage() {
+    console.log(newAverage);
+
+  }
+
+}
