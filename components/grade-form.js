3d98a60172f6c56192cc0eb1325d3174fee60264
Ryan Griego <ryangriego@gmail.com>
2020-05-09 17:36:57 +0000

    updated fixes from pr

--- components/grade-form.js
+++ components/grade-form.js
@@ -1,27 +1,20 @@
 class GradeForm {
   constructor(formElement) {
     this.formElement = formElement;
-
     this.handleSubmit = this.handleSubmit.bind(this);
     this.formElement.addEventListener('submit', this.handleSubmit);
-
   }
 
   onSubmit(createGrade) {
     this.createGrade = createGrade;
-
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    console.log("hi");
     var formData = new FormData(event.target);
     var getDataName = formData.get('name');
     var getDataCourse = formData.get('course');
     var getDataGrade = formData.get('grade');
-
-
-
     this.createGrade(getDataName, getDataCourse, getDataGrade);
     event.target.reset();
 
