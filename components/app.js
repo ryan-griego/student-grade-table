dbc29a35b4c80c12c029f25403cba2ec3137894c
Ryan Griego <ryangriego@gmail.com>
2020-05-09 18:06:46 +0000

    updated html table - added new methods to app class and binded those methods

--- components/app.js
+++ components/app.js
@@ -9,7 +9,9 @@ class App {
     this.createGrade = this.createGrade.bind(this);
     this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
     this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
-
+    this.deleteGrade = this.deleteGrade.bind(this);
+    this.handleDeleteGradeError = this.handleDeleteGradeError.bind(this);
+    this.handleDeleteGradeSuccess = this.handleDeleteGradeSuccess.bind(this);
   }
 
   handleGetGradesError(error) {
@@ -70,6 +72,20 @@ class App {
   }
 
   handleCreateGradeSuccess() {
+    this.getGrades();
+  }
+
+  deleteGrade(id){
+    console.log(id);
+
+  }
+
+  handleDeleteGradeError(error){
+    console.error(error);
+
+  }
+
+  handleDeleteGradeSuccess() {
     this.getGrades();
   }
 
