dd850b6f31351f52ca1a9cbba65fc975153fd864
Ryan Griego <ryangriego@gmail.com>
2020-05-27 00:54:59 +0000

    Completed user can update a grade

--- components/grade-form.js
+++ components/grade-form.js
@@ -25,49 +25,9 @@
       this.createGrade(getDataName, getDataCourse, getDataGrade);
     }
     else {
-
       this.editGrade(getDataName, getDataCourse, getDataGrade);
       document.getElementById('add').value = 'Add';
     }
-
     event.target.reset();
-
-
-
-    // if(checkEdit) {
-    //   // edit a grade to the table
-    //   // NEEDS TO PASS IN THE ID OF THE CLICKED STUDENT
-    //   this.editGrade(id,getDataName, getDataCourse, getDataGrade);
-
-    //   // event.target.reset();
-
-    //   console.log("edit grade function did run");
-    //   console.log("ID of clicked student", id);
-    //   $('.add').text("Add");
-    //   $('.add-text').text("Add grade");
-    //   $('#Name').val("");
-    //   $('#Course').val("");
-
-    //   $('#Grade').val("");
-
-    //   newApp.isEdit = false;
-
-    //   newGradeTable.updateGrades(id, getDataName, getDataCourse, getDataGrade)
-    //   console.log("the current status of edit happens after editgrade", checkEdit);
-
-    // }
-
-    // else{
-    //   // edit the current grade in the table
-    //   this.createGrade(getDataName, getDataCourse, getDataGrade);
-    //   event.target.reset();
-    //   console.log("the current status of edit happens after creatgrade", checkEdit);
-
-
-    // }
-
-
-
-
   }
 }
